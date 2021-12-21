33c8e94b6159dc0a43a891e96305c3a8ac4953c4
sruthin <sruthin@enfintechnologies.com>
2021-12-21 22:55:55 +0530

    initial

---
+++ terraform/production.tf
@@ -0,0 +1,8 @@
+module "networking" {
+source = "./modules/networking"
+  region               = "${var.region}"
+  environment          = "${var.environment}"
+  vpc_cidr             = "${var.vpc_cidr}"
+  public_subnets_cidr  = "${var.public_subnets_cidr}"
+  availability_zones   = "${local.production_availability_zones}"
+}
